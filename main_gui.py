181be5aaed675de9e8fbe26c69042f2c7ec453f7
lincoln-lm <73306575+Lincoln-LM@users.noreply.github.com>
2022-11-30 05:19:16 -0700

    basic settings

--- main_gui.py
+++ main_gui.py
@@ -1,8 +1,13 @@
 """Live Map GUI"""
 
+import os.path
+from typing import Any
+import json
 import customtkinter
 from sv_live_map_core.nxreader import NXReader
 from sv_live_map_core.paldea_map_view import PaldeaMapView
+
+DEFAULT_IP = "192.168.0.0"
 
 customtkinter.set_default_color_theme("blue")
 customtkinter.set_appearance_mode("dark")
@@ -18,6 +23,12 @@ class Application(customtkinter.CTk):
 
         # initialize for later
         self.reader: NXReader = None
+        self.settings: dict[str, Any] = {}
+
+        # if settings file exists then access it
+        if os.path.exists("settings.json"):
+            with open("settings.json", "r", encoding = "utf-8") as settings_file:
+                self.settings = json.load(settings_file)
 
         # window settings
         self.title(self.APP_NAME)
@@ -30,53 +41,34 @@ class Application(customtkinter.CTk):
         self.bind("<Command-w>", self.on_closing)
         self.createcommand('tk::mac::Quit', self.on_closing)
 
-        self.settings_frame = customtkinter.CTkFrame(
-            master = self,
-            width = 150,
-            corner_radius = 0,
-            fg_color = None,
-        )
+        # initialize widgets
+        self.settings_frame = customtkinter.CTkFrame(master = self, width = 150)
+        self.settings_frame.grid(row = 0, column = 0, columnspan = 2, sticky = "nsew")
 
-        self.settings_frame.grid(
-            row = 0,
-            column = 1,
-            rowspan = 1,
-            pady = 0,
-            padx = 0,
-            sticky = "nsew"
-        )
+        self.ip_label = customtkinter.CTkLabel(master = self.settings_frame, text = "IP Address:")
+        self.ip_label.grid(row = 0, column = 0)
 
-        self.map_frame = customtkinter.CTkFrame(
-            master = self,
-            width = 150,
-            corner_radius = 0,
-            fg_color = None,
-        )
+        self.ip_entry = customtkinter.CTkEntry(master = self.settings_frame)
+        self.ip_entry.grid(row = 0, column = 1)
+        self.ip_entry.insert(0, self.settings.get("IP", DEFAULT_IP))
 
-        self.map_frame.grid(
-            row = 0,
-            column = 2,
-            rowspan = 1,
-            pady = 0,
-            padx = 0,
-            sticky = "nsew"
-        )
+        self.map_frame = customtkinter.CTkFrame(master = self, width = 150)
+        self.map_frame.grid(row = 0, column = 2, sticky = "nsew")
 
         self.map_widget = PaldeaMapView(self.map_frame)
-        self.map_widget.grid(
-            row = 1,
-            rowspan = 1,
-            column = 0,
-            columnspan = 1,
-            sticky = "nw",
-            padx = (0, 0),
-            pady = (0, 0)
-        )
+        self.map_widget.grid(row = 1, column = 0, sticky = "nw")
 
     def on_closing(self, _ = None):
         """Handle closing of the application"""
+        # save settings on termination
+        with open("settings.json", "w+", encoding = "utf-8") as settings_file:
+            self.settings['IP'] = self.ip_entry.get()
+            json.dump(self.settings, settings_file)
+
+        # close reader on termination
         if self.reader:
             self.reader.close()
+
         self.destroy()
 
 def main():
