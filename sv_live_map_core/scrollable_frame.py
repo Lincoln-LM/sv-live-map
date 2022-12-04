fa3f2f60e59d30a1d2df20273769ef3a85c665f4
lincoln-lm <73306575+Lincoln-LM@users.noreply.github.com>
2022-12-04 06:57:29 -0700

    allow scrolling while hovering over ScrollableFrame

--- sv_live_map_core/scrollable_frame.py
+++ sv_live_map_core/scrollable_frame.py
@@ -26,9 +26,24 @@ class ScrollableFrame(customtkinter.CTkFrame):
             )
         )
 
+        self.scrollable_frame.bind('<Enter>', self.on_enter)
+        self.scrollable_frame.bind('<Leave>', self.on_leave)
+
         self.canvas_inner.create_window((0, 0), window = self.scrollable_frame, anchor = "center")
 
         self.canvas_inner.configure(yscrollcommand = self.scrollbar.set)
 
         self.canvas_inner.pack(side = "left", fill = "both", expand = True)
         self.scrollbar.pack(side = "right", fill = "y")
+
+    def on_enter(self, _):
+        """On enter event"""
+        self.scrollable_frame.bind_all("<MouseWheel>", self.on_scroll)
+
+    def on_leave(self, _):
+        """On leave event"""
+        self.scrollable_frame.unbind_all("<MouseWheel>")
+
+    def on_scroll(self, event):
+        """On scroll event"""
+        self.canvas_inner.yview_scroll(-1 * (round(event.delta / 120)), "units")
